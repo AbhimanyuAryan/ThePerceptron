148b87a7a19fdfbcea718e1ce0f34a115efce925
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2018-09-13 17:01:27 +0530

    creating perceptron from scratch
    Writing a perceptron to perform XOR

---
+++ Assets/Perceptron.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic; 
+using UnityEngine;
+
+[System.Serializable]
+public class TrainingSet
+{
+	public double[] input;
+	public double output;
+}
+
+public class Perceptron : MonoBehaviour
+{
+	public TrainingSet[] ts;	
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
